31df7da366f2bcf667038fa6c86b0bf5b1a77f2d
Gpere1ra <74246462+Gpere1ra@users.noreply.github.com>
2021-05-18 17:02:45 +0100

    meio feito

--- src/main/kotlin/Position.kt
+++ src/main/kotlin/Position.kt
@@ -1,1 +1,21 @@
-data class Position(val x:Int)
+import pt.isel.canvas.*
+
+data class Position(val x:Int,val y:Int)
+
+enum class Direction(val dx:Int, val dy:Int) {
+    LEFT(-1,0), UP(0,-1), RIGHT(1,0), DOWN(0,1)
+}
+
+fun Direction?.dx() = this?.dx ?: 0
+
+fun Direction?.dy() = this?.dy ?: 0
+
+fun directionOf( key:Int ) :Direction? = when (key) {
+    LEFT_CODE -> Direction.LEFT
+    RIGHT_CODE -> Direction.RIGHT
+    UP_CODE -> Direction.UP
+    DOWN_CODE -> Direction.DOWN
+    else -> null
+}
+
+val ALL_POSITIONS :List<Position>  = (0 until GRID_HEIGHT*GRID_WIDTH).map { Position( it%GRID_WIDTH, it/GRID_WIDTH ) }
--- src/main/kotlin/Snake.kt
+++ src/main/kotlin/Snake.kt
@@ -1,16 +1,34 @@
 import pt.isel.canvas.*
 
-const val CELL_SIDE = 32
-const val GRID_WIDTH = 20
-const val GRID_HEIGHT = 16
-//data class Game(val snake:Snake, val wall:List<Position>)
+data class SnakePos(val x:Int, val y:Int)
+data class Snake(val HeadPos:SnakePos,val TailPos:SnakePos,val Step:Int = 0,val motion:Direction?)
 
-fun main (){
-    onStart{
-        val cv = Canvas(CELL_SIDE*GRID_WIDTH, CELL_SIDE*GRID_HEIGHT, BLACK)
+operator fun SnakePos.plus( dir:Direction ) = Position( x + dir.dx(), y + dir.dy() )
 
+fun Position.isValid(xSize:Int, ySize:Int) = x in 0 until xSize && y in 0 until ySize
+
+fun Canvas.drawSnake(s:Snake) {
+    val hx = s.HeadPos.x * CELL_SIDE - s.Step * s.motion.dx()
+    val hy = s.HeadPos.y * CELL_SIDE - s.Step * s.motion.dy()
+    val tx = s.TailPos.x * CELL_SIDE - s.Step * s.motion.dx()
+    val ty = s.TailPos.y * CELL_SIDE - s.Step * s.motion.dy()
+    val hxImg = SPRITE_DIV * when(s.motion) {
+        Direction.LEFT ,Direction.UP-> 3
+        Direction.RIGHT, Direction.DOWN, null-> 4
     }
-    onFinish {
-        println("2 done 1 to go!")
+    val hyImg = SPRITE_DIV * when(s.motion) {
+        Direction.LEFT, Direction.DOWN -> 1
+        Direction.UP, Direction.RIGHT, null -> 0
     }
+    val txImg = SPRITE_DIV * when(s.motion) {
+        Direction.LEFT, Direction.UP -> 3
+        Direction.RIGHT, Direction.DOWN, null -> 4
+    }
+    val tyImg = SPRITE_DIV * when(s.motion) {
+        Direction.LEFT, Direction.DOWN -> 3
+        Direction.UP, Direction.RIGHT, null -> 2
+    }
+    drawImage("snake.png|$hxImg,$hyImg,$SPRITE_DIV,$SPRITE_DIV",hx,hy,CELL_SIDE,CELL_SIDE)
+    drawImage("snake.png|$txImg,$tyImg,$SPRITE_DIV,$SPRITE_DIV",tx,ty,CELL_SIDE,CELL_SIDE)
 }
+
